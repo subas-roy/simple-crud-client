08599b55f11557c1f20e300bdda5b679c5dce3cd
subas-roy <subasroy46@gmail.com>
2024-10-08 21:55:31 +0600

    Project and Router setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Helmet } from 'react-helmet'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Helmet>
+      <title>Simple CRUD</title>
+    </Helmet>
+      <h1>Simple CRUD</h1>
+    </>
+  )
+}
+
+export default App
