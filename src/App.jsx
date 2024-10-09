185e6e62aaff8d64490548357866b4792aab57cb
subas-roy <subasroy46@gmail.com>
2024-10-09 23:37:39 +0600

    Create client side post and Insert single user data

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-
 import { Helmet } from 'react-helmet'
 import './App.css'
 
 function App() {
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user) // send data to body of server side
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+  }
 
   return (
     <>
-    <Helmet>
-      <title>Simple CRUD</title>
-    </Helmet>
+      <Helmet>
+        <title>Simple CRUD</title>
+      </Helmet>
       <h1>Simple CRUD</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add" />
+      </form>
     </>
   )
 }
